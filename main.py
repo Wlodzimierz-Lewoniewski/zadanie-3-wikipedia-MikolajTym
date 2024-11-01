9518f7d6d6af1283cac716ff6711d81e632da65b
MikolajTym <mikolaj.tym@gmail.com>
2024-11-01 15:58:10 +0100

    U

--- main.py
+++ main.py
@@ -7,34 +7,39 @@ category = input().strip()
 category = category.replace(" ", "_")
 category = quote(category)
 
-category = f"/Kategoria:{category}" # convert to encoded HTML
+category = f"/Kategoria:{category}"
 wiki_url = "https://pl.wikipedia.org/wiki"
 response = requests.get(wiki_url+category).text
 
+main_content = response[re.search(re.compile(r'mw-category-generated'), response).start():]
 p = re.compile(r'<a.+href="\/wiki([^:]+?)".+?>(.+)<\/a>')
-links = re.findall(p, response)[1:3]
+links = re.findall(p, main_content)[:2]
 
 for link in links:
     p = re.compile(r'<li>.*?</li>')
-    links = re.findall(p, response)[:2]
-    links = [re.findall(re.compile(r'href="(.*?)".*?>(.*?)<\/a>'), link) for link in links]
+    links_p = re.findall(p, response)[:2]
+    links_p = [re.findall(re.compile(r'href="(.*?)".*?>(.*?)<\/a>'), link) for link in links_p]
 
     response_1 = requests.get(wiki_url+link[0]).text
     start = re.search(re.compile(r'id="mw-content-text"'), response_1).start()
     text = response_1[start:]
-    links = re.findall(re.compile(r'href="[^:]+?".+?title="(.*?)"'), text)[:5]
+    links_p = re.findall(re.compile(r'href="[^:]+?".+?title="(.*?)"'), text)[:5]
     print(" | ".join(links))
 
     images = re.findall(re.compile(r'<img.+?src="(.+?)"'), text)[:3]
     print(" | ".join(images))
 
-    start = re.search(re.compile(r'id="Przypisy"'), text).start()
-    sources = text[start:]
-    links = re.findall(re.compile(r'href="(http.+?)"'), sources)[:3]
-    print(" | ".join(links))
+    start = re.search(re.compile(r'id="Przypisy"'), text)
+    if start is not None: 
+        sources = text[start.start():]
+        links_p = re.findall(re.compile(r'class="reference-text".+?href="(http.+?)".+?<\/span>'), sources)
+        if len(links_p) > 3:
+            links_p = links_p[:3]
+        print(" | ".join(links_p))
 
-    start = re.search(re.compile('id="catlinks"'), text).start()
-    categories = text[start:]
-    categories = re.findall(re.compile(r'<li.+?>(.+?)</a></li>'), categories)[:3]
+    categories_text = re.search(re.compile('mw-normal-catlinks(.+?)</div>'), text).group()
+    categories = re.findall(re.compile(r'<li.+?>(.+?)</a></li>'), categories_text)
+    if len(categories) > 3:
+        categories = categories[:3]
     print(" | ".join(categories))
     
